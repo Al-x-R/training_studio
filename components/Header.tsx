d3feb652fb6cad24b79e67c6aadccf99d2ff53cc
Alexander Roshchin <alexander.roshchin85@gmail.com>
2022-04-15 23:50:45 +0300

    connected chakra ui

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
+const Header = () => {
+  return (
+    <div>
+      <Button>test</Button>
+    </div>
+  );
+};
+
+export default Header;
