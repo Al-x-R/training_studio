e7a682e0c5cdba7ccd27487b6b943b92472e7201
Alexander Roshchin <alexander.roshchin85@gmail.com>
2022-04-16 21:08:14 +0300

    Added a basic view of the main page

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import React, { FC } from 'react';
+import { Flex, List, ListItem, Text } from '@chakra-ui/react';
 
-const Header = () => {
+const menuList = [
+  {id: 1, title: 'Главня'},
+  {id: 2, title: 'О нас'},
+  {id: 3, title: 'Секции'},
+  {id: 4, title: 'Расписания'},
+  {id: 5, title: 'Цены'},
+]
+
+interface IMenuList {
+  id: number;
+  title: string;
+}
+
+const Header: FC<IMenuList> = ({id, title}) => {
   return (
-    <div>
-      <Button>test</Button>
-    </div>
+    <Flex w={'100%'} h={20} justify={'space-between'} px={10} backgroundColor={'rgba(250, 250, 250, 0.1)'}>
+      <Text fontSize="lg">Logo</Text>
+      <List d={'flex'}>
+        {menuList.map(item => (
+          <ListItem key={item.id} px={2}>{item.title}</ListItem>
+        ))}
+      </List>
+    </Flex>
   );
 };
 
---
+++ helpers/helpers.ts
@@ -0,0 +1,14 @@
+export const NAV_ITEMS = [
+  {id: 1, title: 'home', name: 'Главня', link: '/'},
+  {id: 2, title: 'about us', name: 'О нас', link: ''},
+  {id: 3, title: 'classes', name: 'Секции', link: ''},
+  {id: 4, title: 'schedules', name: 'Расписания', link: ''},
+  {id: 5, title: 'prices', name: 'Цены', link: ''},
+]
+
+// NAV_ITEMS = [
+//   { name: 'Food & Drink', title: 'food-drink', link: '/category/food-drink' },
+//   { name: 'Lifestyle', title: 'lifestyle', link: '/category/lifestyle' },
+//   { name: 'People', title: 'people', link: '/category/people' },
+//   { name: 'Places', title: 'places', link: '/category/places' },
+// ];
